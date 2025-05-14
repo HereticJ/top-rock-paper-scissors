3a95d8a15c7f385b786c3647525d1357890564a7
HereticJ <joshgcallahan@gmail.com>
2025-05-13 17:16:50 -0700

    Updated code.js and index.html to add buttons and UI to Rock Paper Scissors.

--- code.js
+++ code.js
@@ -1,70 +1,159 @@
-// Chooses random float as the computer's choice.
-let computerAnswer = Math.random();
+// Title Page Text
+title = document.querySelector("h1");
+title.style["text-align", "center"];
 
-// Prompts user to enter their choice.
-let humanAnswer = prompt("Enter your choice of rock, paper, or scissors.");
+// Prompts user to make their choice.
+const choicePrompt = document.querySelector("p");
+const prompt = document.createElement("p");
+choicePrompt.textContent = "Select your choice:";
 
-// Initializes human score, starting from 0.
+// Declares answers as blank string.
+let humanSelection = "";
+let computerSelection = "";
+
+// Initializes round counter, starting with one.
+let roundNum = 1;
+
+// Initializes human and computer scores.
 let humanScore = 0;
-
-// Initializes computer score, starting from 0.
 let computerScore = 0;
 
+// Adds score and round counters to UI
+const scores = document.querySelector("ul");
+const score1 = document.createElement("li");
+const score2 = document.createElement("li");
+const currentRound = document.createElement("li");
+currentRound.textContent = `Round: ${roundNum}`;
 
-// Takes random float between 0 and 1 to assign rock, paper, scissors as the computer's choice.
-function getComputerChoice() {
-    if (computerAnswer <= 0.34) {
-        console.log("Computer chooses rock");
-    } else if (computerAnswer > 0.34 && computerAnswer <= 0.67) {
-        console.log("Computer chooses paper");
-    } else if (computerAnswer > 0.67) {
-        console.log("Computer chooses scissors");
-    }
-}
+scores.appendChild(score1);
+scores.appendChild(score2);
+scores.appendChild(currentRound);
 
-// Prompts user to input "rock," "paper," or "scissors" to play against computer.
-function getHumanChoice() {
-    if (humanAnswer.toLowerCase() === "rock") {
-        console.log("You chose rock.");
-    } else if (humanAnswer.toLowerCase() === "paper") {
-        console.log("You chose paper.");
-    } else if (humanAnswer.toLowerCase() === "scissors") {
-        console.log("You chose scissors.");
-    } else {
-        console.log("I didn't quite catch that.")
-    }
-}
+score1.textContent = `Player Score: ${humanScore}`;
+score2.textContent = `Computer Score: ${computerScore}`;
 
-// Compares human answer to computer answer to evaluate who won the round.
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice <= 0.34) {
-        console.log("It's a tie.");
-    } else if (humanChoice === "rock" && (computerChoice > 0.34) && (computerChoice <= 0.67)) {
-        console.log("Paper beats rock. Computer wins!");
+// Uses RNG to determine CPU answer
+function getComputerSelection() {
+    let computerAnswer = Math.random();
+        if (computerAnswer <= 0.34) {
+            computerSelection = "rock";
+        } else if (computerAnswer > 0.34 && computerAnswer <= 0.67) {
+           computerSelection = "paper";
+        } else if (computerAnswer > 0.67) {
+            computerSelection = "scissors";
+        }
+};   
+
+// Adds Rock button for user selection and runs playRound() when clicked.
+const buttons = document.querySelector("#buttons");
+const rockBtn = document.createElement("button");
+
+rockBtn.textContent = "Rock";
+rockBtn.addEventListener("click", () => {
+    humanSelection = "rock";
+        getComputerSelection();
+        playRound(humanSelection, computerSelection);
+    });
+
+// Adds Paper button for user selection and runs RPS round when clicked.
+const paperBtn = document.createElement("button");
+paperBtn.textContent = "Paper";
+paperBtn.addEventListener("click", () => {
+    humanSelection = "paper";
+        getComputerSelection();
+        playRound(humanSelection, computerSelection);
+    });
+
+// Adds Scissors button for user selection and runs RPS round when clicked.
+const scissorsBtn = document.createElement("button");
+
+scissorsBtn.textContent = "Scissors";
+scissorsBtn.addEventListener("click", () => {
+    humanSelection = "scissors";
+        getComputerSelection();
+        playRound(humanSelection, computerSelection);
+    });
+
+// Adds UI buttons
+buttons.appendChild(rockBtn);
+buttons.appendChild(paperBtn);
+buttons.appendChild(scissorsBtn);
+
+// Styles buttons.
+rockBtn.setAttribute("style", "color: red;",);
+rockBtn.style.padding = "15px";
+rockBtn.style.margin = "15px";
+paperBtn.setAttribute("style", "color: blue;");
+paperBtn.style.padding = "15px";
+paperBtn.style.margin = "15px";
+scissorsBtn.setAttribute("style", "color: green;");
+scissorsBtn.style.padding = "15px";
+scissorsBtn.style.margin = "15px";
+
+// Logs results of rounds
+const log = document.querySelector("#log");
+const addLog = document.createElement("p");
+
+    
+function playRound(choice1, choice2) {
+    currentRound.textContent = `Round: ${roundNum}`;
+
+    // Compares selections for each round to determine the round winner.
+    if (choice1 === choice2) {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. Round ${roundNum} ends in a tie.`
+        log.appendChild(addLog);
+        roundNum++;
+    } else if (choice1 === "rock" && choice2 === "paper") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. paper beats rock. Computer wins round ${roundNum}!`;
+        log.appendChild(addLog);
         computerScore++;
-    } else if (humanChoice === "rock" && computerChoice > 0.67) {
-        console.log("Rock beats scissors. Player wins!");
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    } else if (choice1 === "rock" && choice2 === "scissors") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. rock beats scissors. Player wins round ${roundNum}!`;
+        log.appendChild(addLog);
         humanScore++;
-    } else if (humanChoice === "paper" && computerChoice <= 0.34) {
-        console.log("Paper beats rock. Player wins!");
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    } else if (humanSelection === "paper" && computerSelection === "rock") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. paper beats rock. Player wins round ${roundNum}!`;
+        log.appendChild(addLog);
         humanScore++;
-    } else if (humanChoice === "paper" && (computerChoice > 0.34) && (computerChoice <= 0.67)) {
-        console.log("It's a tie.");
-    } else if (humanChoice === "paper" && computerChoice > 0.67) {
-        console.log("Scissors beats paper. Computer wins!");
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    } else if (humanSelection === "paper" && computerSelection === "scissors") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. scissors beats paper. Computer wins round ${roundNum}!`;
+        log.appendChild(addLog);
         computerScore++;
-    } else if (humanChoice === "scissors" && computerChoice <= 0.34) {
-        console.log("Rock beats paper. Computer Wins!");
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    } else if (humanSelection === "scissors" && computerSelection === "rock") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. rock beats scissors. Computer Wins round ${roundNum}!`;
+        log.appendChild(addLog);
         computerScore++;
-    } else if (humanChoice === "scissors" && (computerChoice > 0.34) && (computerChoice <= 0.67)) {
-        console.log("Scissors beats paper. Player wins!");
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    } else if (humanSelection === "scissors" && computerSelection === "paper") {
+        addLog.textContent = `You chose ${choice1}, CPU chose ${choice2}. scissors beats paper. Player wins round ${roundNum}!`;
+        log.appendChild(addLog);
         humanScore++;
-    } else if (humanChoice === "scissors" && computerScore > 0.67) {
-        console.log("It's a tie.");
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+        roundNum++;
+        score1.textContent = `Player Score: ${humanScore}`;
+        score2.textContent = `Computer Score: ${computerScore}`;
+    };
+        // Evaluates scores to determine the overall winner.
+        if (humanScore === 5 && computerScore < 5) {
+            addLog.textContent = `Your scored 5 points, you win!`;
+            log.appendChild(addLog);
+            buttons.remove();
+        } else if (computerScore === 5 && humanScore < 5) {
+            addLog.textContent = "Computer scored 5 points, you lose!"
+            log.appendChild(addLog);
+            buttons.remove();
+        };
+};
